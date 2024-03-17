b66ee82ef6dfd1ecc15440b65ddee96d67fb0909
ashwithaa-lgit config --global user.email ashwithaa.l2022cse@sece.ac.in <ashwithaa.l2022cse@sece.ac.in>
2024-03-17 21:29:51 +0530

    incomplete

--- frontend/my-react-app/src/App.jsx
+++ frontend/my-react-app/src/App.jsx
@@ -1,16 +1,19 @@
 import Eventpage from "./components/Eventpage"
 import Registration from "./components/Registration"
-import Login from "./components/Login"
-// import Aboutus from "./components/Aboutus"
+// import Login from "./components/Login"
+import Aboutus from "./components/Aboutus"
+import Profile from "./components/Profile"
 
 
 
 
 const App=()=>{
   return (<>
-  <Eventpage/>
-  {/* <Aboutus/> */}
-  {/*<Registration/>*/}
+  {/* <Eventpage/> */}
+  {/* <Aboutus/>  */}
+  <Registration/>
+  {/* <Profile/> */}
+  {/* <Login/> */}
  
   </>)
 }
